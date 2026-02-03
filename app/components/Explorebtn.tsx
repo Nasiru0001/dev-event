9393ebef29194eac2ef896aac4fac140b5cb096d
Nas <adamunasiru935@gmail.com>
2026-02-03 16:22:32 +0100

    first commit

---
+++ app/components/Explorebtn.tsx
@@ -0,0 +1,27 @@
+"use client";
+import Image from "next/image";
+
+const button = () => {
+  return (
+    <div className=" flex items-center justify-center">
+      <button
+        type="button"
+        className="mt-7 mx-auto "
+        onClick={() => console.log("Hello Baby")}
+        id="explore-btn"
+      >
+        <a href="#events">Explore Events</a>
+
+        <Image
+          src="./icons/arrow-down.svg"
+          width={24}
+          height={24}
+          priority
+          alt="arrow-down"
+        />
+      </button>
+    </div>
+  );
+};
+
+export default button;
